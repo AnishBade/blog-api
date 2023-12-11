4743b5eb7dc617aa8c21ffa6a6d8b7c90ae0bd51
Anish Bade <badeanish6045@gmail.com>
2023-12-11 14:59:18 +0545

    cors and csrf features added

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     "posts.apps.PostsConfig",  # new
     # 3rd party apps
     "rest_framework",
+    "corsheader",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -133,3 +135,9 @@ REST_FRAMEWORK = {  # new
         "rest_framework.permissions.AllowAny",
     ],
 }
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]

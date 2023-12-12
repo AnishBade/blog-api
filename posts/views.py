411d2b56ee48ff9a8fb3ed63ef0b3221afcbd486
Anish Bade <badeanish6045@gmail.com>
2023-12-12 10:41:19 +0545

    user endpoints added

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 
 class PostList(generics.ListCreateAPIView):
@@ -14,3 +15,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnly,)
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
